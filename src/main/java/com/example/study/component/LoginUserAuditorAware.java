2e7ae22f1d13d520188f3b212e00403cb430efd6
Aminoragit <wjsxkxk1@gmail.com>
2021-04-03 16:28:09 +0900

    문제해결 : jpaAuditingHandler 에러 발생 -> config/JpaConfig가 중복될경우 발생하는 에러 해결방법 : 작성한 config폴더째로 JpaConfig를 삭제해준다 -> 최신 스프링부트에서 기본으로 제공하므로 굳이 작성할 필요없음 결과 : AdminUser create()정상작동

--- src/main/java/com/example/study/component/LoginUserAuditorAware.java
+++ src/main/java/com/example/study/component/LoginUserAuditorAware.java
@@ -1,16 +1,14 @@
-package com.example.study.componant;
-
+package com.example.study.component;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.stereotype.Component;
-
 import java.util.Optional;
 
 @Component
 public class LoginUserAuditorAware implements AuditorAware<String> {
 
     @Override
-    public Optional<String> getCurrentAuditor(){
-        return Optional.of("AdminServer"); //로그인 시스템이 없어서 일단 기본값설정
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("AdminServer");	//로그인 시스템이 없어서 일단 기본값설정
     }
 }
--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -27,10 +31,19 @@ public class User {
     private String phoneNumber;
     private LocalDateTime registeredAt;
     private LocalDateTime unregisteredAt;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @CreatedBy
     private String createdBy;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    @LastModifiedBy
     private String updatedBy;
+
 
     // User 1 : N OrderGroup
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
