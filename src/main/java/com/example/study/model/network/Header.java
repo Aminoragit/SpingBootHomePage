bcdd04b915b9bb73f9c2056b4bd7c4cd6bc832ea
Aminoragit <wjsxkxk1@gmail.com>
2021-04-03 17:02:08 +0900

    getController PostController 롤백

--- src/main/java/com/example/study/model/network/Header.java
+++ src/main/java/com/example/study/model/network/Header.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public aspect Header {
+public class Header {
 
     // api 통신시간
     private String transactionTime;
