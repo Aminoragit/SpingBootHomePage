b797dbd05b353b9db7855dcc7893b62188e8833c
Aminoragit <wjsxkxk1@gmail.com>
2021-04-09 14:43:14 +0900

    9-03 Controller 추상화 적용하기 1) 000Controller들은  CrudInterface를 상속받는데
    @RequestBody나 @Putmapping처럼 중복되는 부분이고 개발자의 실수로 미입력시 서비스가 작동되지 않는문제가 있다
    이걸 방지해주기 위해 CrudContorller을 작성해준다.
    
    2)작성한 CrudCotroller들을 각 00Controller에서 extends로 상속받는다.
    
    3) 이렇게 하면 아래 동일하게 작성한 controller들을 다시 적어줄 필요가 없다.
    
    4) @PostConstruct
           public void init(){this. this.baseService=0000ApiLogicService;}}
     을 추가하여 어떤 로직서비스로 연결되는지 알려줘야한다.

--- src/main/java/com/example/study/controller/api/UserApiController.java
+++ src/main/java/com/example/study/controller/api/UserApiController.java
@@ -1,6 +1,7 @@
 package com.example.study.controller.api;
 
 
+import com.example.study.controller.CrudController;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
@@ -10,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 
 @Slf4j
 @RestController
@@ -18,55 +21,60 @@ import org.springframework.web.bind.annotation.*;
 //일일히 CRUD를 설정해주는건 귀찮으니
 //ifs->CrudInterface 인터페이스 작성
 //각 Crud의 매핑설정
-public class UserApiController implements CrudInterface<UserApiRequest,UserApiResponse> {
+public class UserApiController extends CrudController<UserApiRequest,UserApiResponse> {
 
     @Autowired
     private UserApiLogicService userApiLogicService;
 
-    @Override
-    @PostMapping("/user") // /api/user
-    public Header<UserApiResponse>
-        create(@RequestBody Header<UserApiRequest> request)
-    {
-        //System.out.println과 비슷한 동작을 한다.
-        log.info("{},{}",request,"ABC");
-        return userApiLogicService.create(request);
+    @PostConstruct
+    public void init(){
+        this.baseService=userApiLogicService;
     }
-
-    @Override
-    @GetMapping("/user/{id}")
-    public Header<UserApiResponse>
-        read(@PathVariable(name="id") Long id) {
-        log.info("read id : {}",id);
-        return userApiLogicService.read(id);
-    }
-
-    @Override
-    @PutMapping("/user")
-    public Header<UserApiResponse>
-        update(@RequestBody Header<UserApiRequest> request) {
-
-        return userApiLogicService.update(request);
-    }
-
-
-
-    @Override
-    @DeleteMapping("/user/{id}")
-    public Header
-        delete(@PathVariable(name="id") Long id) {
-        log.info("delete : {}",id);
-        return userApiLogicService.delete(id);
-    }
-
-    @PostMapping("/user2") // /api/user2
-    public Header<UserApiResponse>
-        checkEmailCreate(@RequestBody Header<UserApiRequest> request)
-    {
-        //System.out.println과 비슷한 동작을 한다.
-        log.info("{},{}",request,"ABC");
-        return userApiLogicService.checkEmailCreate(request);
-    }
+//
+//    @Override
+//    @PostMapping("/user") // /api/user
+//    public Header<UserApiResponse>
+//        create(@RequestBody Header<UserApiRequest> request)
+//    {
+//        //System.out.println과 비슷한 동작을 한다.
+//        log.info("{},{}",request,"ABC");
+//        return userApiLogicService.create(request);
+//    }
+//
+//    @Override
+//    @GetMapping("/user/{id}")
+//    public Header<UserApiResponse>
+//        read(@PathVariable(name="id") Long id) {
+//        log.info("read id : {}",id);
+//        return userApiLogicService.read(id);
+//    }
+//
+//    @Override
+//    @PutMapping("/user")
+//    public Header<UserApiResponse>
+//        update(@RequestBody Header<UserApiRequest> request) {
+//
+//        return userApiLogicService.update(request);
+//    }
+//
+//
+//
+//    @Override
+//    @DeleteMapping("/user/{id}")
+//    public Header
+//        delete(@PathVariable(name="id") Long id) {
+//        log.info("delete : {}",id);
+//        return userApiLogicService.delete(id);
+//    }
+//
+//    @PostMapping("/user2") // /api/user2
+//    public Header<UserApiResponse>
+//        checkEmailCreate(@RequestBody Header<UserApiRequest> request)
+//    {
+//        //System.out.println과 비슷한 동작을 한다.
+//        log.info("{},{}",request,"ABC");
+//        return userApiLogicService.checkEmailCreate(request);
+//    }
 
 
 
