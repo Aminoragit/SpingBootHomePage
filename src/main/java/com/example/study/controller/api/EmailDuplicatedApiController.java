b1566e1e22c6ac9f234858e4bd688e589c653bed
Aminoragit <wjsxkxk1@gmail.com>
2021-04-09 17:30:19 +0900

    09-06 추상화 코드적용하기 partner

---
+++ src/main/java/com/example/study/controller/api/EmailDuplicatedApiController.java
@@ -0,0 +1,17 @@
+package com.example.study.controller.api;
+
+
+import com.example.study.controller.CrudController;
+import com.example.study.model.entity.User;
+import com.example.study.model.network.request.UserApiRequest;
+import com.example.study.model.network.response.UserApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/api/user2")
+public class EmailDuplicatedApiController extends CrudController<UserApiRequest,UserApiResponse, User> {
+}
