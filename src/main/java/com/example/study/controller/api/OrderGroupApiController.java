581b4a41260af35c84ffac1e11c1a5eb2ff7fdb0
Aminoragit <wjsxkxk1@gmail.com>
2021-04-09 12:37:37 +0900

    8-3-01 주문내역 API CREATE

---
+++ src/main/java/com/example/study/controller/api/OrderGroupApiController.java
@@ -0,0 +1,46 @@
+package com.example.study.controller.api;
+
+
+import com.example.study.ifs.CrudInterface;
+import com.example.study.model.network.Header;
+import com.example.study.model.network.request.OrderGroupApiRequest;
+import com.example.study.model.network.response.OrderGroupApiResponse;
+import com.example.study.service.OrderGroupApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/orderGroup")
+public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+    @Autowired
+    private OrderGroupApiLogicService orderGroupApiLogicService;
+
+
+    @Override
+    @PostMapping("")
+    public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
+        return orderGroupApiLogicService.create(request);
+    }
+
+    @Override
+    @GetMapping("{id}")
+    public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+        return null;
+    }
+
+    //RequestBody 입력한 body대로 전달된다는 얘기
+    @Override
+    @PutMapping("")
+    public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
+        return null;
+    }
+
+    //PathVariable=>주소에 써져있는 그대로 진행한다는 얘기
+    @Override
+    @DeleteMapping("{id}")
+    public Header delete(@PathVariable Long id) {
+        return null;
+    }
+}
